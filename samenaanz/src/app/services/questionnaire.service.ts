b99bbaa6de2582237393475cd120335f9f83eb97
mtudorr <m.tudor@student.fontys.nl>
2023-04-11 22:41:13 +0200

    Refresh questionnaires when changed

--- samenaanz/src/app/services/questionnaire.service.ts
+++ samenaanz/src/app/services/questionnaire.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Questionnaire} from "./questionnaire";
 import {Client} from "../api/client";
+import {Observable, Subject} from "rxjs";
+import {Mutation} from "./mutation";
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,15 @@ import {Client} from "../api/client";
 export class QuestionnaireService {
 
   private readonly api: Client;
-
+  private readonly mutationsSubject: Subject<Mutation>;
 
   public constructor(api: Client) {
     this.api = api;
+    this.mutationsSubject = new Subject<Mutation>();
+  }
+
+  public get Mutations(): Observable<Mutation> {
+    return this.mutationsSubject;
   }
 
   public async RetrieveAll(): Promise<readonly Questionnaire[]> {
--- samenaanz/src/app/views/questionnaires/questionnaires.component.ts
+++ samenaanz/src/app/views/questionnaires/questionnaires.component.ts
@@ -1,13 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {QuestionnaireService} from "../../services/questionnaire.service";
 import {Questionnaire} from "../../services/questionnaire";
+import {Mutation} from "../../services/mutation";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-questionnaires',
   templateUrl: './questionnaires.component.html',
   styleUrls: ['./questionnaires.component.scss']
 })
-export class QuestionnairesComponent implements OnInit {
+export class QuestionnairesComponent implements OnInit, OnDestroy {
+
+  private subscription: Subscription | undefined;
 
   private readonly questionnaireService: QuestionnaireService;
   public questionnaires: readonly Questionnaire[];
@@ -17,7 +21,21 @@ export class QuestionnairesComponent implements OnInit {
     this.questionnaires = [];
   }
 
-  public async ngOnInit(): Promise<void> {
+  private async RetrieveQuestionnaires(): Promise<void> {
     this.questionnaires = await this.questionnaireService.RetrieveAll();
+  }
+
+  public async ngOnInit(): Promise<void> {
+    await this.RetrieveQuestionnaires();
+
+    this.subscription = this.questionnaireService.Mutations.subscribe(async m => {
+      if(m === Mutation.Questionnaires) {
+        await this.RetrieveQuestionnaires();
+      }
+    })
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
   }
 }
