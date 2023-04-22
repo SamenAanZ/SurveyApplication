f7e967b36348fc842125bff8f0138b72f79ac8c6
rakker93 <rory_akker@live.nl>
2023-04-22 12:47:31 +0200

    work in progress

--- samenaanz/src/app/app.module.ts
+++ samenaanz/src/app/app.module.ts
@@ -1,12 +1,25 @@
+// Angular Core
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APP_INITIALIZER } from '@angular/core';
 
+// Custom components
 import { AppComponent } from './app.component';
 import { QuestionnairesComponent } from './views/questionnaires/questionnaires.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CreateQuestionnaireComponent } from './views/create-questionnaire/create-questionnaire.component';
-import {FormsModule} from "@angular/forms";
-import { AppRoutingModule } from './app-routing.module';
+
+// Nebula
+import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbButtonModule } from '@nebular/theme';
+import { NbMenuModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+
+// Keycloak
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+import { initializeKeycloak } from './auth/app.init';
 
 @NgModule({
   declarations: [
@@ -18,9 +31,24 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbSidebarModule.forRoot(),
+    NbButtonModule,
+    NbMenuModule.forRoot(),
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+      }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
