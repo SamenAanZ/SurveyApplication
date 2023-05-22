c627b36abc245e847f511309569667a44ed5e10c
rakker93 <rory_akker@live.nl>
2023-05-22 16:56:43 +0200

    Editted profile and questionnaire page

--- samenaanz/src/app/views/profile/profile.component.ts
+++ samenaanz/src/app/views/profile/profile.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  public profile: KeycloakProfile | null = null;
+  
+  constructor(private readonly keycloakService: KeycloakService) {}
 
+  async ngOnInit(): Promise<void> {
+    this.profile = await this.keycloakService.loadUserProfile();
+  }
+
+  public async navigateToProfile(): Promise<void> {
+    await this.keycloakService.getKeycloakInstance().accountManagement();
+  }
 }
